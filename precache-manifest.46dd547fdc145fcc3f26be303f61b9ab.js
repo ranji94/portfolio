74e7aa6bc4314c0469802be99812ef0eda0dffdf
ranji94 <ranji@notowany.pl>
2020-08-24 20:05:19 +0200

    Updates

---
+++ precache-manifest.46dd547fdc145fcc3f26be303f61b9ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238e161d1c4217efba3f3edfead8acff",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "870bcb10d8321f71d6c7",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a115198d750d05629550",
+    "url": "/portfolio/static/js/2.25fb914c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.25fb914c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "870bcb10d8321f71d6c7",
+    "url": "/portfolio/static/js/main.80f30843.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "21096ae11e8245a9882143ad07827d55",
+    "url": "/portfolio/static/media/janusz.21096ae1.jpg"
+  }
+]);
