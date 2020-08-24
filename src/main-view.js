1b8e3f2c62f5dcb0b3a1dfe9af7e7af8e35dc77d
Jędrzej Miłosz Piasecki <jedrzej.piasecki94@gmail.com>
2020-08-24 22:30:35 +0200

    [DEV] Webpack, babel and SASS loaders configured

---
+++ src/main-view.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './styles/main.scss'
+
+class MainView extends Component {
+  render () {
+    return(<div>Example</div>)
+  }
+}
+
+export default MainView;
